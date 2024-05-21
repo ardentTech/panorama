0e58ee034e45e2f34c821f16209a1abf7cfc4a40
Jonathan D. Baker <jonathan.baker@method.com>
2024-05-21 09:31:00 -0600

    clean up panorama entrypoints

--- src/main/kotlin/Panorama.kt
+++ src/main/kotlin/Panorama.kt
@@ -4,9 +4,11 @@ import kotlinx.serialization.json.ClassDiscriminatorMode
 import kotlinx.serialization.json.Json
 import lexicon.*
 import java.nio.file.Path
+import kotlin.Exception
 import kotlin.io.path.*
 
 private val logger = KotlinLogging.logger {}
+private const val JSON_FILE_EXT = "json"
 
 object Panorama {
 
@@ -17,29 +19,36 @@ object Panorama {
         }
     }
 
-    @OptIn(ExperimentalPathApi::class)
-    fun codegen(
-        destination: Path = Path("./src/main/kotlin"),
-        namespaces: List<String>,
-        source: Path = Path("./data"),
-    ) {
-        // TODO should this do `defs.json` and other top-level records, objects and tokens first?
-        val docs = source.walk(PathWalkOption.INCLUDE_DIRECTORIES).filter { it.extension == "json" }.map {
-            json.decodeFromString<LexiconDoc>(it.readText())
-        }.toList()
-
-        namespaces.forEach { namespace ->
-            docs.filter { it.id.startsWith(namespace) }.forEach { doc ->
-                logger.info { "generating code for ${doc.id} lexicon" }
-                doc.codegen(destination)
-            }
-        }
+    fun codegen(doc: LexiconDoc): Result<Unit> {
+        doc.codegen().writeTo(System.out)
+        return Result.success(Unit)
     }
+
+    fun codegen(doc: LexiconDoc, destination: Path): Result<Unit> {
+        doc.codegen().writeTo(destination)
+        return Result.success(Unit)
+    }
+
+    @OptIn(ExperimentalPathApi::class)
+    fun crawl(namespaces: List<String>, source: Path = Path("./data")): Result<List<LexiconDoc>> =
+        try {
+            val docs = source.walk(PathWalkOption.INCLUDE_DIRECTORIES).filter { it.extension == JSON_FILE_EXT }.map {
+                json.decodeFromString<LexiconDoc>(it.readText())
+            }.toList()
+            Result.success(if (namespaces.isNotEmpty()) { docs.filter { it.namespace in namespaces }} else { docs })
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
 }
 
 fun main() {
-    Panorama.codegen(namespaces = listOf(
+    Panorama.crawl(namespaces = listOf(
         //"com.atproto",
-        "app.bsky"
-    ))
+        "app.bsky.actor",
+        "app.bsky.embed",
+        "app.bsky.feed"
+    )).fold(
+        onSuccess = { docs -> docs.forEach { Panorama.codegen(it) }},
+        onFailure = { t -> t.printStackTrace() }
+    )
 }
