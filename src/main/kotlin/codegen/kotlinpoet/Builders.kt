405522e85229af46a2ffad828f614f5f1018988d
Jonathan D. Baker <jonathan.baker@method.com>
2024-05-23 09:57:15 -0600

    add data object codegen wrapper

--- src/main/kotlin/codegen/kotlinpoet/Builders.kt
+++ src/main/kotlin/codegen/kotlinpoet/Builders.kt
@@ -1,10 +1,20 @@
 package codegen.kotlinpoet
 
+import com.squareup.kotlinpoet.KModifier
 import com.squareup.kotlinpoet.TypeSpec
+
+// these functions aim to provide a more friendly API for building KotlinPoet specs
 
 internal fun buildEnum(constants: List<String>, description: String? = null, name: String): TypeSpec {
     val spec = TypeSpec.enumBuilder(name)
     description?.let { spec.addKdoc(it) }
     constants.forEach { spec.addEnumConstant(it) }
+    return spec.build()
+}
+
+internal fun buildDataObject(description: String? = null, name: String): TypeSpec {
+    val spec = TypeSpec.objectBuilder(name)
+        .addModifiers(KModifier.DATA)
+    description?.let { spec.addKdoc(it) }
     return spec.build()
 }
--- src/test/kotlin/codegen/BuildersTest.kt
+++ src/test/kotlin/codegen/BuildersTest.kt
@@ -6,6 +6,29 @@ import kotlin.test.assertEquals
 class BuildersTest {
 
     @Test
+    fun `buildDataObject without description`() {
+        assertEquals("""
+            |public data object FooBar
+            |
+            """.trimMargin(),
+            codegen.kotlinpoet.buildDataObject(name = "FooBar").toString()
+        )
+    }
+
+    @Test
+    fun `buildDataObject with description`() {
+        assertEquals("""
+            |/**
+            | * foobar
+            | */
+            |public data object FooBar
+            |
+            """.trimMargin(),
+            codegen.kotlinpoet.buildDataObject(description = "foobar", name = "FooBar").toString()
+        )
+    }
+
+    @Test
     fun `buildEnum without description`() {
         assertEquals("""
             |public enum class TestEnum {
